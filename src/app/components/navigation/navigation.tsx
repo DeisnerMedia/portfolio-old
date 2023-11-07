5b0ce1afb786bdc558ddaced60d89de2ce63cc3e
DeisnerMedia <aim.gtamodding@gmail.com>
2023-11-07 23:35:16 +0100

    Hamburger Menu now working

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -1,37 +1,182 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/app/assets/images/logo.png';
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden';
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+        document.body.style.overflow = 'auto';
+    };
+
+    useEffect(() => {
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
     return (
         <nav className="text-white">
             <div className="container mx-auto flex justify-between items-center py-8">
-                <div>
+                <div className="ml-10">
                     <Link href="/">
-                        <Image src={Logo} alt="Logo" width={70} height={70} />
+                        <Image src={Logo} alt="Logo" width={70} height={70}/>
                     </Link>
                 </div>
-                <div className="lg:flex space-x-20 text-2xl text-amber-400">
+                <div className="lg:flex space-x-20 text-2xl text-amber-400 navigation-extended">
                     <NavItem href="#home" text="Home" />
                     <NavItem href="#about" text="About" />
                     <NavItem href="#experience" text="Experience" />
                     <NavItem href="#skills" text="Skills" />
                     <NavItem href="#projects" text="Projects" />
                 </div>
+                <svg
+                    id="hamburger"
+                    className={`Header__toggle-svg flex ${isMenuOpen ? 'open' : ''}`}
+                    onClick={toggleMenu}>
+                    <g stroke="#fff" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round">
+                        <path id="top-line" d="M10,10 L50,10 Z"></path>
+                        <path id="middle-line" d="M10,20 L50,20 Z"></path>
+                        <path id="bottom-line" d="M10,30 L50,30 Z"></path>
+                    </g>
+                </svg>
+                <div className={`flex navigation-hamburger text-5xl justify-center items-center flex-col gap-10 ${isMenuOpen ? 'flex' : 'hidden'}`}>
+                    <NavItem href="#home" text="Home" closeMenu={closeMenu}/>
+                    <NavItem href="#about" text="About"  closeMenu={closeMenu}/>
+                    <NavItem href="#experience" text="Experience" closeMenu={closeMenu}/>
+                    <NavItem href="#skills" text="Skills" closeMenu={closeMenu}/>
+                    <NavItem href="#projects" text="Projects" closeMenu={closeMenu}/>
+                </div>
             </div>
             <style jsx>{`
               .navbar-container {
                 position: relative;
               }
+                .navigation-extended {
+                    display: flex;
+                }
+                #hamburger {
+                    display: none;
+                }
+              
+              @media (max-width: 1200px) {
+                .navigation-extended {
+                  display: none;
+                }
+                #hamburger {
+                  display: block;
+                  position: absolute;
+                  top: 30%;
+                  right: 2%;
+                  z-index: 101;
+                  cursor: pointer;
+                }
+
+                .navigation-hamburger {
+                  height: 100vh;
+                  width: 100%;
+                  position: absolute;
+                  right: 0;
+                  top: 0;
+                  backdrop-filter: blur(10px);
+                  background-color: rgba(0, 0, 0, 0.90);
+                  z-index: 100;
+                }
+              
+              svg {
+                    width: 60px;
+                    position: absolute;
+                    top: 30%;
+                    right: 25px;
+                  }
+
+                  #top-line,
+                  #bottom-line,
+                  #middle-line {
+                    transform-box: fill-box;
+                    transform-origin: center;
+                  }
+
+                  svg.open {
+                    #top-line {
+                      animation: ${isMenuOpen ? 'down-rotate 0.6s ease-out both' : 'up-rotate 0.6s ease-out both'};
+                    }
+
+                    #bottom-line {
+                      animation: ${isMenuOpen ? 'up-rotate 0.6s ease-out both' : 'down-rotate 0.6s ease-out both'};
+                    }
+
+                    #middle-line {
+                      animation: hide 0.6s ease-out forwards;
+                    }
+                  }
+
+                  @keyframes up-rotate {
+                    0% {
+                      animation-timing-function: cubic-bezier(0.16, -0.88, 0.97, 0.53);
+                      transform: translateY(0px);
+                    }
+                    30% {
+                      transform-origin: center;
+                      animation-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
+                      transform: translateY(-10px);
+                    }
+                    100% {
+                      transform-origin: center;
+                      transform: translateY(-10px) rotate(45deg) scale(0.9);
+                    }
+                  }
+
+                  @keyframes down-rotate {
+                    0% {
+                      animation-timing-function: cubic-bezier(0.16, -0.88, 0.97, 0.53);
+                      transform: translateY(0px);
+                    }
+                    30% {
+                      transform-origin: center;
+                      animation-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
+                      transform: translateY(10px);
+                    }
+                    100% {
+                      transform-origin: center;
+                      transform: translateY(10px) rotate(-45deg) scale(0.9);
+                    }
+                  }
+
+                  @keyframes hide {
+                    29% {
+                      opacity: 1;
+                    }
+                    30% {
+                      opacity: 0;
+                    }
+                    100% {
+                      opacity: 0;
+                    }
+                  }
+                }
             `}</style>
         </nav>
+
     );
 }
 
-function NavItem({ href, text }: { href: string; text: string }) {
+function NavItem({ href, text, closeMenu }: { href: string; text: string; closeMenu?: () => void }) {
+    const handleClick = () => {
+        if (closeMenu) {
+            closeMenu();
+        }
+    };
+
     return (
-        <Link href={href} className="hover:text-yellow-300 font-bold">
+        <Link href={href} className="hover:text-yellow-300 font-bold" onClick={handleClick}>
             {text}
         </Link>
     );
--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -17,19 +17,23 @@ export default function Home() {
                     Fullstack Developer since <span className="text-3xl md:text-4xl font-bold bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-500 text-transparent bg-clip-text">2020</span>
                 </h2>
                 <div className="text-left mb-4 mt-8">
-                    <button className="bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-400 hover:from-amber-500 hover:via-orange-300 hover:to-yellow-500 text-black text-2xl md:text-2xl font-bold py-3 w-52 md:w-52 rounded-md mr-10 mb-4 inline-block">
-                        <a href="#about">About Me</a>
-                    </button>
-                    <button className="bg-transparent hover:bg-amber-500 hover:text-white hover:outline-0 text-amber-500 text-2xl md:text-2xl font-bold py-3 w-52 md:w-52 outline outline-amber-600 rounded-md inline-block">
-                        Contact
-                    </button>
+                    <a href="#about">
+                        <button className="bg-gradient-to-r from-amber-600 via-orange-400 to-yellow-400 hover:from-amber-500 hover:via-orange-300 hover:to-yellow-500 text-black text-2xl md:text-2xl font-bold py-3 w-52 md:w-52 rounded-md mr-10 mb-4 inline-block">
+                            About Me
+                        </button>
+                    </a>
+                    <a href="mailto:contract@deisnermedia.com">
+                        <button className="bg-transparent hover:bg-amber-500 hover:text-white hover:outline-0 text-amber-500 text-2xl md:text-2xl font-bold py-3 w-52 md:w-52 outline outline-amber-600 rounded-md inline-block">
+                            Contact
+                        </button>
+                    </a>
                 </div>
             </div>
             <div className="overflow-hidden hidden md:block text-right" style={{ zIndex: 1, maxWidth: '100%', position: 'absolute', right: 0 }}>
                 <Spline scene="https://prod.spline.design/yoQ4BaAHXEuUokSd/scene.splinecode" style={{ width: '1000px', height: '700px' }} />
             </div>
 
-            <div className="absolute inset-x-0 bottom-32 flex justify-center" style={{ zIndex: 3 }}>
+            <div className="absolute inset-x-0 bottom-32 flex justify-center" style={{ zIndex: 2 }}>
                 <div className="flex justify-center space-x-20">
                     <a href="https://www.linkedin.com/in/dominik-deisner/" target="_blank" rel="noopener noreferrer">
                         <FaLinkedin className="text-7xl text-gray-300 hover:text-blue-500" />
