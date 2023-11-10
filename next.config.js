ac9cadca4f4c83f147dc6b1757728dcafc9c5b6d
DeisnerMedia <aim.gtamodding@gmail.com>
2023-11-10 22:36:07 +0100

    Workflow Changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     images: {
         minimumCacheTTL: 60,
         domains: ['github.com', 'via.placeholder.com'],
         loader: 'default',
+        unoptimized: true,
     }
 }
 
